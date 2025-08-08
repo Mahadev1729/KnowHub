0c1645d72af2afaacb3e64f03bb082ebbe9a5db8
Mahadev1729 <devcampus17@gmail.com>
2025-08-08 22:38:03 +0530

    i got error cabout crispy

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . models import Notes
+from . forms import *
 
 # Create your views here.
 
@@ -7,8 +7,9 @@ def home(request):
     return render(request,'dashboard/home.html')
 
 def notes(request):
+    form=NotesForm()
     notes=Notes.objects.filter(user=request.user)
-    context={'notes':notes}
+    context={'notes':notes,'form':form}
     return render(request,'dashboard/notes.html',context)
 
 
